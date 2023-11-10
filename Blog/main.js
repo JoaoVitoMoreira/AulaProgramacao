6130630ccda6dfc48679e1c0442cd043d1371be4
JoaoVitoMoreira <ens-joaomoreira@ugv.edu.br>
2023-11-10 20:41:50 -0300

    a

--- Blog/main.js
+++ Blog/main.js
@@ -1,3 +1,7 @@
-function trocaimg() {
-    
+function valida() {
+    if(volta.checked){
+        window.location.href="../index.html"
+    }else{
+        alert("Marque a opção para voltar para a página inicial")
+    }
 }
