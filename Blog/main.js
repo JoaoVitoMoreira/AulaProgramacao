8e924ea0f03e760e0dc02bdf67980c84464d92c5
JoaoVitoMoreira <ens-joaomoreira@ugv.edu.br>
2023-11-11 12:37:30 -0300

    final

--- Blog/main.js
+++ Blog/main.js
@@ -5,3 +5,26 @@ function valida() {
         alert("Marque a opção para voltar para a página inicial")
     }
 }
+
+function entrar() {
+    let resp = window.confirm("Então você prefere backend?")
+    if(resp==true){
+        window.location.href="content/node.html"
+    }else{
+        window.location.href="index.html"
+    }
+}
+
+function trocaimg() {
+    switch(passatempo.value){
+        case "frifa":
+            document.getElementById("imagem").innerHTML="<img src='imagens/frifa.jpg'>"
+        break;
+        case "mineirin":
+            document.getElementById("imagem").innerHTML="<img src='imagens/mineirinho.jpg'>"   
+        break;  
+        case "sonic" :
+            document.getElementById("imagem").innerHTML="<img src='imagens/sonic.jpeg'>"
+        break;        
+    }
+}
